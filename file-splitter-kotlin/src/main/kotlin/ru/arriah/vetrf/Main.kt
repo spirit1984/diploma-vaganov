3d9a3b2866fc9ed3dc5a6fc192c493c87582dc18
Shevchenko Dmitry <vdshevch296@googlemail.com>
2017-06-16 14:23:46 +0300

    Создан скелет проекта по разделению файлов - но на языке Kotlin

---
+++ file-splitter-kotlin/src/main/kotlin/ru/arriah/vetrf/Main.kt
@@ -0,0 +1,8 @@
+package ru.arriah.vetrf
+
+/**
+ * Created by shevchenko-dv-100705 on 16.06.17.
+ */
+fun main(args: Array<String>) {
+    println("This is the stub for kotlin program")
+}
---
+++ file-splitter-kotlin/src/test/kotlin/ru/arriah/vetrf/HelloTest.kt
@@ -0,0 +1,15 @@
+package ru.arriah.vetrf
+
+import org.junit.Assert
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+    @Test
+    fun simpleTestJustToMakeSureItWorks() {
+        Assert.assertTrue(true)
+    }
+
+
+}
