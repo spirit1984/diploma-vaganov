b99a79eed0d5410cfed99b9f54d0b516e27a1a3b
Shevchenko Dmitry <vdshevch296@googlemail.com>
2017-06-28 16:48:01 +0300

    Описано почти все из самой фазы вычисления, за исключением того, как вычисляется окончание строки

--- string-matcher-kotlin/src/main/kotlin/ru/arriah/vetrf/Main.kt
+++ string-matcher-kotlin/src/main/kotlin/ru/arriah/vetrf/Main.kt
@@ -1,5 +1,11 @@
 package ru.arriah.vetrf
 
+import sun.text.normalizer.UTF16.append
+import java.awt.SystemColor.text
+import javax.swing.text.StringContent
+
+
+
 /**
  * Created by 123 on 21.06.2017.
  */
@@ -22,6 +28,7 @@ data class ApproximateMatch(val start: Int, val end: Int, val distance:Int,
 
 enum class DIRECTION {UP, LEFT,DIAG, SKIP, UNEXPECTED}
 data class Cell(val value: Short = 0, val direction: DIRECTION = DIRECTION.UNEXPECTED, val calculated: Boolean = false)
+data class StringBufferContent(val pattern: StringBuffer = StringBuffer(), val text: StringBuffer = StringBuffer())
 
 /**
  * Данный класс используется как промежуточные, и не используется для вывода напрямую пользователю
@@ -58,8 +65,9 @@ fun analyzeDetailed(pattern: String, text: String, quickMatch: QuickApproximateM
 }
 
 fun addOffset(analyzeDetailed: ApproximateMatch, left: Int): ApproximateMatch =
-        ApproximateMatch(start = analyzeDetailed.start+left-1, end = analyzeDetailed.end+left-1,
-                distance = analyzeDetailed.distance, text = analyzeDetailed.text, pattern =  analyzeDetailed.pattern)
+        analyzeDetailed.copy(start = analyzeDetailed.start+left-1, end = analyzeDetailed.end+left-1)
+
+
 
 fun analyzeDetailed(pattern: String, text: String): ApproximateMatch {
     val (n, m) = Pair(pattern.length, text.length)
@@ -68,8 +76,60 @@ fun analyzeDetailed(pattern: String, text: String): ApproximateMatch {
     for (row in 0..n)
         for (col in 0..m)
             calcCell(matr, row, col, pattern, text)
+    return convertMatrixToResponse(matr, pattern, text)
+}
 
-    TODO("not implemented")
+fun convertMatrixToResponse(matr: Array<Array<Cell>>, pattern: String, text: String): ApproximateMatch {
+    val col = getEditDistancePosition(matr, pattern, text)
+    val n = pattern.length
+    val distance = matr[n][col].value
+    val content = getStrings(matr, pattern, text)
+    val offset = calculateOffset(content.pattern.toString())
+    return ApproximateMatch(text = content.text.toString(), pattern = content.pattern.toString(),
+            distance = distance.toInt(), start = offset, end = calculateEnd())
+}
+
+fun calculateEnd(): Int {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
+
+fun  getStrings(matr: Array<Array<Cell>>, pattern: String, text: String): StringBufferContent {
+    val col = getEditDistancePosition(matr, pattern, text)
+    val result = getStrings(matr, pattern.length, col, pattern, text)
+
+    val m = text.length
+    for (i in (col+1)..m) {
+        result.text.append(text[i-1])
+        result.pattern.append(MISS_SIGN)
+    }
+    return result
+
+}
+
+fun append(content: StringBufferContent, pattern: Char, text: Char): StringBufferContent {
+    content.pattern.append(pattern)
+    content.text.append(text)
+    return content
+}
+
+fun  getStrings(matr: Array<Array<Cell>>, row: Int, col: Int, pattern: String, text: String): StringBufferContent {
+    if (row == 0 && col == 0) return StringBufferContent()
+    val textSymbol = text[col-1]
+    if (row == 0) return append(getStrings(matr, row, col-1, pattern, text), textSymbol, MISS_SIGN)
+    val patternSymbol = pattern[row-1]
+    if (matr[row][col].direction == DIRECTION.DIAG) {
+        return append(getStrings(matr, row-1, col-1, pattern, text), textSymbol, patternSymbol);
+
+    }
+    if (matr[row][col].direction == DIRECTION.UP) {
+        return append(getStrings(matr, row-1, col, pattern, text), GAP_SIGN, patternSymbol);
+    }
+
+    if (matr[row][col].direction == DIRECTION.LEFT) {
+        return append(getStrings(matr, row, col-1, pattern, text), textSymbol, GAP_SIGN);
+    }
+
+    TODO("Never should have got here") //To change body of created functions use File | Settings | File Templates.
 }
 
 fun getEditDistancePosition(matr: Array<Array<Cell>>, pattern: String, text: String): Int {
@@ -118,7 +178,7 @@ fun analyzeQuick(pattern: String, text: String): QuickApproximateMatch {
 
     for (x in 1..n) {
         val patternSymbol = pattern[x-1] // Не забываем про смещение
-        matrix.second[0] = x // Получить Получить непустой образец из пустой строки можно только за x - извиняйте
+        matrix.second[0] = x // Получить непустой образец из пустой строки можно только за x - извиняйте
         for (y in 1..m) {
             val textSymbol = text[y-1] // Не забываем про смещение
             matrix.second[y] = minInt(1+matrix.second[y-1], 1 + matrix.first[y],
